738636819de122b0f58f4258a062a42647834a76
TheLifeEducation <159277472+TheLifeEducation@users.noreply.github.com>
2024-02-07 16:41:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-})
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
 
-window.onscroll = function() {scrollFunction()};
+// Close the mobile menu when a link is clicked
+document.querySelectorAll('.navbar__links').forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('is-active');
+        menuLinks.classList.remove('active');
+    });
+});
+
+window.onscroll = function() {
+    scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("goTopBtn").style.display = "block";
-  } else {
-    document.getElementById("goTopBtn").style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("goTopBtn").style.display = "block";
+    } else {
+        document.getElementById("goTopBtn").style.display = "none";
+    }
 }
 
 function goToTop() {
-  document.body.scrollTop = 0; /* For Safari */
-  document.documentElement.scrollTop = 0; /* For Chrome, Firefox, IE and Opera */
+    document.body.scrollTop = 0; /* For Safari */
+    document.documentElement.scrollTop = 0; /* For Chrome, Firefox, IE and Opera */
 }
